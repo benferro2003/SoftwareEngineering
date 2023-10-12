9911df58f9c83bb4a495f83dcc5c1ecce11556ed
benferro2003 <74298155+benferro2003@users.noreply.github.com>
2023-10-12 12:06:21 +0100

    Create Counter.java

---
+++ Counter.java
@@ -0,0 +1,8 @@
+package c3654717;
+
+public interface Counter {
+	int countWords(String sentence); // counts the words in the sentence
+	int countLetters(String sentence); // counts the letters in the sentence
+	int getLength(String sentence); //gets length of the string
+
+}
