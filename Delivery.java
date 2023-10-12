73ae97b909483bca637724c0bf03bbe1a9842314
benferro2003 <74298155+benferro2003@users.noreply.github.com>
2023-10-12 13:45:44 +0100

    Create Delivery.java
    Lab4

---
+++ Delivery.java
@@ -0,0 +1,11 @@
+package c3654717;
+
+public interface Delivery 
+{
+	public String getTutor();
+	public void setTutor(String tutor);
+	public String getRoom();
+	public void setRoom(String room);
+	
+
+}
